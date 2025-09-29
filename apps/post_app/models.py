23c44a10ce77493895b669285f9827163396a1b6
Nikita Lugachev <nikitalugachev149th@gmail.com>
2025-09-29 20:09:50 +0700

    Update PostImage upload_to path

--- apps/post_app/models.py
+++ apps/post_app/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
 
 class PostImage(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to="posts/")
+    image = models.ImageField(upload_to="")
 
     def __str__(self):
         return f"Image for post {self.post.id}"
